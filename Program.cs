a4eab04ebc5867f89895726a15c7db884f201b26
Flexshu <egor.wm202@gmail.com>
2025-08-30 11:39:44 +0300

    V1

---
+++ Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace DelegatesDemo
+{
+    delegate double MyOperation(double a, double b);
+
+    class Program
+    {
+        static double Calculate(double x, double y, MyOperation op)
+        {
+            return op(x, y);
+        }
+
+        static void Main()
+        {
+            MyOperation add = (a, b) => a + b;
+            MyOperation sub = (a, b) => a - b;
+            MyOperation mul = (a, b) => a * b;
+            MyOperation div = (a, b) => a / b;
+
+            Console.WriteLine("Add: " + Calculate(10, 5, add));
+            Console.WriteLine("Subtract: " + Calculate(10, 5, sub));
+            Console.WriteLine("Multiply: " + Calculate(10, 5, mul));
+            Console.WriteLine("Divide: " + Calculate(10, 5, div));
+        }
+    }
+}
